bf2702545bcfc97f8e8c091eb7a62a85435cfaf9
yerin_J <86339431+jiyerin@users.noreply.github.com>
2023-07-10 23:29:32 +0900

    main:html,css,js

---
+++ app.js
@@ -0,0 +1,110 @@
+const todoInput = document.querySelector("input");
+const todo = document.querySelector(".todo-box");
+const submitBtn = document.getElementById("submitbtn");
+
+let todoList = [];
+
+// todoList라는 리스트를 만들어 입력하게 될 리스트를 저장함.
+function setting(){
+    loadStorage();
+    submitBtn.addEventListener("click", createList);
+}
+setting();
+
+//위에서 저장한 것을 불러오기
+function loadStorage()
+{
+    const storedTodo = localStorage.getItem("TODO");
+    if(storedTodo != null)
+    {
+        const myTodoList = JSON.parse(storedTodo);
+        myTodoList.forEach( todo => {
+            const{text} = todo;
+            const {checked} = todo;
+            printTodo(text, checked);
+            storeTodo(text, checked);
+        });
+    }
+}
+
+//작성한 todo리스트 배열 만들기
+function createList(e)
+{
+    e.preventDefault();
+    const todoValue = todoInput.value;
+    if(todoValue =="")
+        alert("할 일을 입력해주세요.")
+    else
+    {
+        printTodo(todoValue, 0);
+        storeTodo(todoValue, 0);
+        todoInput.value = "";
+    }
+}
+
+
+//localStorage에 저장하기
+//localStorage에 object를 저장할 것 -> object를 생성하는 과정이다. (todoListObj가 object)
+function storeTodo(todoValue, checkValue){
+    const todoListObj = {
+        text : todoValue,
+        id : todoList.length + 1,
+        checked : checkValue,
+    };
+    todoList.push(todoListObj);
+    localStorage.setItem("TODO", JSON.stringify(todoList)); //자바스크리트의 값을 문자열로 만들어서 localStorage에 저장. (stringify 사용)
+}
+
+//화면에 list 나타내기!
+function printTodo(todoValue, checkValue)  
+{
+    const li = document.createElement("li");
+    const   span = document.createElement( "span");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "삭제";
+    if (checkValue == 1)
+    {
+        span.InnerHTML = todoValue;
+        li.appendChild(span);
+        li.appendChild(delBtn);  
+        li.id = todoList.length + 1;
+        li.style.color = "#ccc";
+        li.style.textDecoration="line-through";
+        todo.appendChild(li);
+    }
+    else (checkValue == 0)
+    {
+        span.innerHTML = todoValue;
+        li.appendChild(span);
+        li.appendChild(delBtn); 
+        li.id = todoList.length + 1;
+        todo.appendChild(li);
+    }
+    span.addEventListener("click", checkTodo);
+    delBtn.addEventListener("click", deleteTodo);
+}
+
+//삭제기능 구현하기!
+
+function deleteTodo(e)
+{
+    const {target : button} = e;
+    const li = button.parentNode;
+    todo.removeChild(li);
+    todoList = todoList.filter((todo) => todo.id != Number(li.id));
+    localStorage.setItem("TODO", JSON.stringify(todoList));
+}
+
+//완료 todo 체크표시하기
+function checkTodo(e)
+{
+    const {target : span} = e;
+    const li =  span.parentNode;
+    li.style.color = "#ccc";
+    li.style.textDecoration="line-through";
+    todoList.forEach( currentElement => {
+        if(currentElement.id ==  Number(li.id))
+            currentElement.checked = 1;
+    });
+    localStorage.setItem("TODO", JSON.stringify(todoList));
+}
