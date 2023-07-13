07c05b75685d26c08c3acaf93b8ab60b73bebd9b
Shin Yujin <syj021231@naver.com>
2023-07-13 11:37:27 +0900

    modify hamburger slider

--- CODE/mainscreen/script.js
+++ CODE/mainscreen/script.js
@@ -1,7 +1,4 @@
 const goal= document.getElementById("number").innerText * 1;
-const btn = document.getElementById("side-bar-btn");
-const bar = document.getElementById("side-bar");
-const barWidth = bar.style.width;
 const saveBtn = document.getElementById("saveBtn");
 const memoArea = document.getElementById("memo");
 let isShowing = false;
@@ -25,31 +22,6 @@ $({ val : 0 }).animate({ val : goal }, {
     - step : val 값에 따라 스텝별로 애니메이션을 진행하는 함수
     - complete : 애니메이션이 끝난 후 실행될 함수
 */
-
-// 사이드 바
-
-function showBar(){
-    if(isShowing){
-        isShowing = false;
-        bar.style.transform = "translateX(100px)";
-        bar.style.transition = "transform .5s ease-out";
-        /*
-        bar.style.setProperty("transform", "translateX(100px)");
-        bar.style.setProperty("transition", "transform .5s ease-out");
-        */
-        bar.style.display = "none";
-    }
-    else{
-        isShowing = true;
-        bar.style.display = "block";
-        bar.style.transform = "translateX(0px)";
-        bar.style.transition = "transform .5s ease-out";
-       /*
-        bar.style.setProperty("transform", "translateX(0px)");
-        bar.style.setProperty("transition", "transform .5s ease-out");
-        */
-    }
-}
 
 // 메모 관련
 
