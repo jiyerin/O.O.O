7ae663a78ed3fa972fe0e2aa1ea9a0831b5b491c
Shin Yujin <syj021231@naver.com>
2023-07-17 16:00:38 +0900

    login function

--- CODE/homescreen/home.js
+++ CODE/homescreen/home.js
@@ -6,3 +6,22 @@ $(document).ready(function(){
         $("#popup").fadeOut();
     });
 })
+
+// 로그인 기능
+
+const form = document.getElementById("form");
+const submit = document.getElementById("submit");
+let pID;
+let pPW;
+
+function checkLogin(){
+    pID = document.getElementById("input-id").value;
+    pPW = document.getElementById("input-pw").value;
+    console.log("id : " + pID);
+    console.log("pw : " + pPW);
+    if( (pID == "ooo" || pID == "OOO") && (pPW == 1234) ){
+        location.href = "../mainscreen/main.html";
+    }else{
+        alert("로그인에 실패하셨습니다.");
+    }
+}
